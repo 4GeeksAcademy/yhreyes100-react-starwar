333233801f4b54b85f2dfa011f8813ca940dcf7b
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-09-17 01:45:04 +0000

    commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,82 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const apiUrl="https://www.swapi.tech/api/";
+	const [people,setPeople]=useState([]);
+	const [planets,setPlanets]=useState([]);
+	useEffect(()=>{
+		function getPeople(){
+			fetch(apiUrl+"people/")
+			.then(res => res.json())
+			.then(data => {
+				setPeople(data.results);
+				console.log(data.results)
+			})
+			.catch(err => console.error(err))
+		}
+		function getPlanets(){
+			fetch(apiUrl+"planets/")
+			.then(res => res.json())
+			.then(data => {
+				setPlanets(data.results)
+				console.log(data.results)
+			})
+			.catch(err => console.error(err))
+		}
+		getPeople();
+		getPlanets();
+	},[]);
+	return (
+		<>
+		<div className="container scr">
+			<h1>Characters</h1>
+			<div className="overflow-auto">
+				<div className="d-flex">
+			{
+				people!=null?	
+				people.map((p,index)=>(
+					<div key={index}>						
+							<div className="card" >
+							<img src={p.url} class="card-img-top" alt="..."/>
+							<div className="card-body">
+							<h5 className="card-title">{p.name}</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="#" class="btn btn-primary">Learn More!</a>
+							</div>
+					 		</div>
+					</div>		
+				)):""
+			}
+				</div>
+			</div>
+		</div>	
+		<div className="container scr">
+			<h1>Planets</h1>
+			<div className="overflow-auto">
+				<div className="d-flex">
+			{
+				planets!=null?	
+				planets.map((p,index)=>(
+					<div key={index}>						
+							<div className="card" >
+							<img src={p.url} class="card-img-top" alt="..."/>
+							<div className="card-body">
+							<h5 className="card-title">{p.name}</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<div className="d-flex space-between">
+							<a href="#" class="btn btn-primary">Learn More!</a>
+							<a><i className="fa-solid fa-heart"></i></a>
+							</div>
+							</div>
+					 		</div>
+					</div>		
+				)):""
+			}
+				</div>
+			</div>
+		</div>
+		</>
+	);
+}
